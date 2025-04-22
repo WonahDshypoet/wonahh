3a5f92fa29867a84a9a6b4374892ef3ff36f575f
Wonahh <wonahgodwin40@gmail.com>
2025-04-22 13:36:17 -0700

    gdh

--- wonahhh/wonahhh/settings.py
+++ wonahhh/wonahhh/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = FALSE
 
 ALLOWED_HOSTS = [
     'https://wonah-portfolio.onrender.com/'
